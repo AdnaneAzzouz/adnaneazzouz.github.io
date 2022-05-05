2a762349370f07ae5265be97e02173226739e713
AdnaneAzzouz <59777419+AdnaneAzzouz@users.noreply.github.com>
2022-05-05 16:44:31 +0200

    initial commit
    Le VRAI portfolio

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Intro</a>
+        <a class="nav-link" href="#">Qualité</a>
+        <a class="nav-link" href="#">Compétences</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+<div class="card text-center">
+  <h5 class="fw-bold fs-1 card-header">PORTFOLIO</h5>
+  <div class="card-body">
+  </div>
+</div>
+
+<div class="card text-center">
+  <div class="card-body">
+    <h5 class="card-title">INTRO</h5>
+    <p class="card-text">Je suis Adnane Azzouz, 19 ans, actuellement en 2ème année de Brevet de Technicien Supérieur : Services informatique aux organisations. Je suis dans la filiere Solution Logiciels et Applications Metier.</p>
+    <p class="card-text">Je suis parti dans cette filiere car dejà depuis petit, j'ai été fasciné a l'esprit que de simples morceaux de codes permettait de faire enormément de choses et j'ai voulu absolument faire parti de ce groupe
+      de personnes ayant acquis cette abilité.</p>
+    <p class="card-text">Le simple fait que, la maintenant, ce que j'utilise en informatique, ce que je REGARDE dans mon téléphone, mon ordinateur, ma télé connécté... voir que beaucoup est disponible grace a du codage... beaucoup
+      de motivation s'est accru en moi et m'a poussé a avancer maintenant dans cette filiere</p>
+  </div>
+</div>
+
+<div class="card text-center">
+  <div class="card-body">
+    <h5 class="card-title">QUALITE</h5>
+    <p class="card-text">●Je suis très patient quand il faut coder. Je saurais essayer et reessayer et reessayer jusqu'a ce que les problemes soient résolus et que le travail donné soit rendus parfaitement comme attendu.</p>
+    <p class="card-text">●Je sais rechercher de manière efficace les informations sur le langage de programmation si jamais je bloque ou si jamais je ne connais pas. Grâce a celà, le travail peut continuer sans trop de blocage.</p>
+    <p class="card-text">●Je suis expliquer mon travail en detail. Malgré mes bégaillements, mes explications peuvent être compris et claire. En cas de non-compréhension, mon travail est TOUJOURS documenté.</p>
+  </div>
+</div>
+
+<div class="card text-center">
+  <div class="card-body">
+    <h5 class="card-title">COMPETENCES</h5>
+    <p class="card-text">●Je sais coder avec le langage C# et Java, deux langages etant utilisé pour du codage logiciel.</p>
+    <p class="card-text">●Je sais coder avec le langage HTML, CSS et PHP et dans cette logique, je suis apte a travailler en tant de developpeur WEB.</p>
+    <p class="card-text">●Je sais coder avec le langage JSON pour des requetes HTTPs</p>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
