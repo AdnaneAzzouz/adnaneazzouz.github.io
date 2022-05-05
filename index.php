146b043a6733864026b9e2eebe3250a1c9e34627
AdnaneAzzouz <59777419+AdnaneAzzouz@users.noreply.github.com>
2022-05-05 22:50:01 +0200

    Better

--- index.php
+++ index.php
@@ -16,47 +16,73 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Intro</a>
-        <a class="nav-link" href="#">Qualité</a>
-        <a class="nav-link" href="#">Compétences</a>
+        <a class="nav-link active" aria-current="page" href="#intro">Intro</a>
+        <a class="nav-link" href="#formation">Ecoles</a>
+        <a class="nav-link" href="#qualites">Qualité</a>
+        <a class="nav-link" href="#competences">Compétences</a>
+        <a class="nav-link" href="#stages">Stages</a>
       </div>
     </div>
   </div>
 </nav>
 
 
-<div class="card text-center">
-  <h5 class="fw-bold fs-1 card-header">PORTFOLIO</h5>
-  <div class="card-body">
+<div>
+  <h5 class="fw-bold fs-1 text-center">PORTFOLIO</h5>
   </div>
 </div>
 
-<div class="card text-center">
+<div class="card" id="intro">
   <div class="card-body">
-    <h5 class="card-title">INTRO</h5>
+    <h5 class="card-title card-header text-center">INTRO</h5>
     <p class="card-text">Je suis Adnane Azzouz, 19 ans, actuellement en 2ème année de Brevet de Technicien Supérieur : Services informatique aux organisations. Je suis dans la filiere Solution Logiciels et Applications Metier.</p>
     <p class="card-text">Je suis parti dans cette filiere car dejà depuis petit, j'ai été fasciné a l'esprit que de simples morceaux de codes permettait de faire enormément de choses et j'ai voulu absolument faire parti de ce groupe
-      de personnes ayant acquis cette abilité.</p>
+      de personnes ayant acquis cette abilité. Mon oncle, developpeur WEB, est le premier qui a accentué cette sensation.</p>
     <p class="card-text">Le simple fait que, la maintenant, ce que j'utilise en informatique, ce que je REGARDE dans mon téléphone, mon ordinateur, ma télé connécté... voir que beaucoup est disponible grace a du codage... beaucoup
       de motivation s'est accru en moi et m'a poussé a avancer maintenant dans cette filiere</p>
   </div>
 </div>
 
-<div class="card text-center">
+<div class="card" id="formation">
   <div class="card-body">
-    <h5 class="card-title">QUALITE</h5>
+    <h5 class="card-title card-header text-center">Formation (écoles) et ambition future</h5>
+    <p class="card-text">En premier lieu, j'ai fait un BAC STMG (je voulais faire un BAC S mais les portes me l'ont été fermé) option SIG. Dans cette formation, j'ai pu apprendre Python, HTML et CSS. Ayant acquis cette logique, j'ai
+      été avantagé au cours de mes années de BTS et en stage.
+    </p>
+    <p class="card-text">Après avoir eu mon BAC STMG, je ne me sentais pas encore prêt. C'est pourquoi j'ai opté pour un BTS SIO en deux ans afin d'y voir plus clair sur le sujet du developpement et je n'ai pas été déçu. Dans cette formation,
+      j'ai appris les languages suivants: C#, + de python, Java, JavaScript, JSON et PHP. En deux ans (donc une de spécialité), j'ai eu plus confiance en moi pour le monde du travail et acquis des competences que je ne pensais pas pouvoir gagné. 
+      Cependant, cela n'est pas suffisant. Si j'ai la possibilité d'accroître mes connaisances en developpement, je continuerais et c'est ce que je compte faire</p>
+    <p class="card-text">En effet, mon but est de pouvoir évoluer au maximum afin de pouvoir me sentir prêt a intégrer n'importe quel boîte qui recherche du developpement WEB ou CLIENT.</p>
+  </div>
+</div>
+
+<div class="card" id="qualites">
+  <div class="card-body">
+    <h5 class="card-title card-header text-center">QUALITES</h5>
     <p class="card-text">●Je suis très patient quand il faut coder. Je saurais essayer et reessayer et reessayer jusqu'a ce que les problemes soient résolus et que le travail donné soit rendus parfaitement comme attendu.</p>
     <p class="card-text">●Je sais rechercher de manière efficace les informations sur le langage de programmation si jamais je bloque ou si jamais je ne connais pas. Grâce a celà, le travail peut continuer sans trop de blocage.</p>
     <p class="card-text">●Je suis expliquer mon travail en detail. Malgré mes bégaillements, mes explications peuvent être compris et claire. En cas de non-compréhension, mon travail est TOUJOURS documenté.</p>
   </div>
 </div>
 
-<div class="card text-center">
+<div class="card" id="competences">
   <div class="card-body">
-    <h5 class="card-title">COMPETENCES</h5>
+    <h5 class="card-title card-header text-center">COMPETENCES</h5>
     <p class="card-text">●Je sais coder avec le langage C# et Java, deux langages etant utilisé pour du codage logiciel.</p>
     <p class="card-text">●Je sais coder avec le langage HTML, CSS et PHP et dans cette logique, je suis apte a travailler en tant de developpeur WEB.</p>
     <p class="card-text">●Je sais coder avec le langage JSON pour des requetes HTTPs</p>
+  </div>
+</div>
+
+<div class="card" id="stages">
+  <div class="card-body">
+    <h5 class="card-title card-header text-center">FORMATION PROFESSIONEL</h5>
+    <p class="card-text">Mes stages ont été effectué a Villeurbanne, dans l'enseigne de la CNAF (Caisse Nationale des Allocations Familiales). C'est un établissement public qui finance l'ensemble des préstations familiales. 
+      La CNAF s'occupe de la branche 'Famille' de la Sécurité sociale, qui est géré par le réseau formé par la CAF (Caisse D'Allocations Familiales)</p>
+    <p class="card-text">Pour éviter que la question soit posé: La difference entre la CAF et la CNAF est que l'un (CNAF) est complementaire a la CAF. Elle est aussi limité géographiquement, dont le <b>N</b> dans C<b>N</b>AF</p>
+    <br>
+    <p class="card-text">Mes stages ont consisté a créer, dans les 2 stages, un logiciel metier dans <a href="https://powerapps.microsoft.com">PowerApps</a> pour afficher un tableau a l'interieur, modifiable et triable. les tabeaux ressemblent a <a href="images/PartiePowerAppsAffichage.png">CELA</a></p>
+    <p class="card-text">Dans le deuxieme stage, ca a plus été sur les flux Power Automate ou je devais creer un flux d'historisation: <a href="images/HistorisationPartie1.gif">Partie 1</a>, <a href="images/HistorisationPartie2.gif">Partie 2</a> et <a href="images/HistorisationPartie3.gif">Partie 3</a></p>
   </div>
 </div>
 
